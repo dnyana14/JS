a4e6886b43aa165c90e38c8bf144c6f7fb68ba7e
Dnyaneshwari Gaikwad <156452859+dnyana14@users.noreply.github.com>
2025-05-15 06:54:44 +0000

    Data_types

---
+++ Basic/data_types.js
@@ -0,0 +1,14 @@
+"use Strict" // treat all code  with newer version ........not necessary to declare beacuse it auttomaticaaly goes to new version of js
+
+//alert("Hello World") //we are using nodejs not browser........thats why it shows error
+
+let name = "Dnyana"    // string = ""
+let age = 22
+let isStudent = true   // boolean = true/false
+
+let acc = null // null = no value
+
+let state; // undefined = no value(not assigned yet)
+
+console.log(typeof name); 
+console.log(typeof age);
