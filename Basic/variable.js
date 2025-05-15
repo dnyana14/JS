aac4a5289dac61f62227769ef016d8e5f095ef65
Dnyaneshwari Gaikwad <156452859+dnyana14@users.noreply.github.com>
2025-05-15 06:43:32 +0000

    Varibles of JS

---
+++ Basic/variable.js
@@ -0,0 +1,11 @@
+const accountID = 1234567890;
+let AccountEmail = "Dnyana@gmail.com"
+let AccountPassword = "Dnyana@123"     //best way to declare var
+var account = "Dnyana" // not the way to declare variables
+AccountCity = "Pune"      // not the way to declare variables
+console.log(accountID);
+console.log(AccountEmail);
+console.log(AccountPassword);
+console.log(AccountCity);
+
+console.table([AccountEmail, accountID, AccountPassword, AccountCity]);  //table will be created
