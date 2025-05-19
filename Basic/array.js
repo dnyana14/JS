59704362ac0e838fc0866fa37403e01a461e37c4
Dnyaneshwari Gaikwad <156452859+dnyana14@users.noreply.github.com>
2025-05-19 04:09:23 +0000

    Slicing & splicing of an array

--- Basic/array.js
+++ Basic/array.js
@@ -2,3 +2,26 @@ const newarray = [2, 3, 4, 5, 6, 7, 8, 9, 10 , true, "hii"]; // we can add any t
 
 const newarray1 = new Array(2, 3, 4, 5); 
 console.log(newarray1[2])
+
+//array methods
+
+//const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // array of numbers
+array.push(11); // add element at the end of array
+array.push(12);
+array.pop(); // remove last element of array
+console.log(array)
+
+array.unshift(0); // add element at the start of array , all array elements will shift to right
+console.log(array)
+array.shift(); // remove first element of array
+console.log(array)
+
+console.log(array.includes(2)); // check if element is present in array
+console.log(array.indexOf(2)); // check index of element in array
+console.log(array.lastIndexOf(3)); // check last index of element in array
+
+const array1 = array.join()
+console.log(array1) // convert array to string
+console.log(array)
+console.log(typeof array1) // check type of variable
+
---
+++ Basic/slice.js
@@ -0,0 +1,14 @@
+// array slice and splice
+//Slice is used to return a section of array without modifying the original array
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+console.log("A ", array)
+
+const slicearray = array.slice(1 , 3)// slice array from index 1 to 3 , it will not include index 3 but include index 1
+console.log(slicearray) 
+console.log( "B",array) 
+
+//Splice is used to add or remove elements from array
+const splicearray = array.splice(1 , 3 ,"hey") // remove 3 elements from index 1 to 3 and add "hey" at index 1
+console.log(splicearray) // removed elements
+console.log( "C",array) // original array is modified
+
