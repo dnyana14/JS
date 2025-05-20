c5c4af1689c38cc4fcc09608047d2b0311b75c0a
Dnyaneshwari Gaikwad <156452859+dnyana14@users.noreply.github.com>
2025-05-20 07:38:44 +0000

    deconstructing

---
+++ Basic/object4.js
@@ -0,0 +1,10 @@
+const course = {
+    title: "JavaScript",
+    duration: 3,
+    price : 200,
+}  
+
+//course.title
+
+const {title} = course   // Destructuring, if you want to get the title property from the course object then you can use destructuring like this
+console.log(title) // JavaScript
